74ec6156b6036449702a9a6402e7f7559cc288c6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-09-10 22:48:27 +0200

    Split into separate file

--- AvaloniaHugeTree/TreeNodeModel.cs
+++ AvaloniaHugeTree/TreeNodeModel.cs
@@ -1,63 +1,8 @@
 using System;
 using System.Collections.Generic;
-using Avalonia.Collections;
-using Avalonia.Threading;
 
 namespace AvaloniaHugeTree
 {
-    public class RootTreeNodeModel : TreeNodeModel
-    {
-        public IAvaloniaReadOnlyList<TreeNodeModel> VisibleChildren => _visibleChildren;
-        private AvaloniaList<TreeNodeModel> _visibleChildren = new AvaloniaList<TreeNodeModel>();
-
-        public RootTreeNodeModel()
-        {
-            _root = new Root(this);
-        }
-        
-        class Root : ITreeNodeRoot
-        {
-            private readonly RootTreeNodeModel _root;
-            private bool _updateEnqueued;
-
-            public Root(RootTreeNodeModel root)
-            {
-                _root = root;
-            }
-
-            public void EnqueueUpdate()
-            {
-                if (!_updateEnqueued)
-                {
-                    _updateEnqueued = true;
-                    Dispatcher.UIThread.Post(Update, DispatcherPriority.Background);
-                }
-            }
-
-            private static void AppendItems(AvaloniaList<TreeNodeModel> list, TreeNodeModel node)
-            {
-                list.Add(node);
-                if(node.IsExpanded)
-                    foreach(var ch in node.Children)
-                        AppendItems(list, ch);
-            }
-
-            public void Update()
-            {
-                _updateEnqueued = false;
-                var list = new AvaloniaList<TreeNodeModel>();
-                AppendItems(list, _root);
-
-                _root._visibleChildren = new AvaloniaList<TreeNodeModel>(list);
-                _root.RaisePropertyChanged(nameof(_root.VisibleChildren));
-            }
-            
-             
-        }
-
-        public void ForceResync() => ((Root) _root).Update();
-    }
-    
     public class TreeNodeModel : PropertyChangedBase
     {
         private bool _isExpanded;
@@ -69,6 +14,7 @@ namespace AvaloniaHugeTree
         protected interface ITreeNodeRoot
         {
             void EnqueueUpdate();
+            void Update();
         }
         
         public bool IsExpanded
@@ -122,6 +68,5 @@ namespace AvaloniaHugeTree
         }
 
         public void AddChild(TreeNodeModel child) => InsertChild(_children.Count, child);
-
     }
 }
